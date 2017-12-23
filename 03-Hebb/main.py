445b8f05323fd4ae5814917c5e3bddd0b2ea40b9
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2017-12-23 20:56:06 +0100

    Refactoring

--- 03-Hebb/main.py
+++ 03-Hebb/main.py
@@ -1,26 +1,38 @@
 from neurons import *
 import numpy as np
 from emojis import *
+from random import randint
 
 #np.random.seed(5)
 neuronGrid = [15, 15]
 lRate=0.007
 fRate=0.4
 
+
 emoji = Emoji()
 
 def countUniqueItems(arr):
     return len(Counter(arr).keys())
 
+def noiseEmojis(arr, numOfPixels):
+    noisedArr = arr[:]
+    for emoji in noisedArr:
+        for _ in range(numOfPixels):
+            emoji[randint(0, 63)] *= -1
+
+    return noisedArr
+
+
 trainingSet = [
     emoji.getEmoji('sad'),
-    emoji.getEmoji('sad_noised'),
     emoji.getEmoji('smile'),
     emoji.getEmoji('angry'),
     emoji.getEmoji('xD'),
     emoji.getEmoji('confused'),
     emoji.getEmoji('test')
 ]
+
+noisedSet = noiseEmojis(trainingSet, 3)
 
 
 hebbGroup = HebbNeuronGroup(
@@ -32,12 +44,9 @@ hebbGroup = HebbNeuronGroup(
     fRate=fRate
 )
 
-# for neuron in hebbNrs:
-#     neuron.setTrainingData(trainingSet)
-
 winners = []
 for i in range(100):
-    winners = hebbGroup.train(trainingSet)
+    winners = hebbGroup.train(trainingSet+noisedSet)
 
 for winner in winners:
     print(winner._iid)
